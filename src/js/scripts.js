1d3bbc1aac25a279bbd038ec8ed87fd91cb779c9
RondneyLoiola <rondney.loiola11@gmail.com>
2025-09-03 16:43:50 -0300

    .

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,48 +1,14 @@
-// Espera até que a página esteja completamente carregada
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
+    const hideAllBoxes = () => document.querySelectorAll('.box').forEach(box => box.classList.remove('ativo'));
 
-    // Seleciona todos os elementos que têm a classe 'player' e os armazena em uma variável chamada 'player'
-    const player = document.querySelectorAll('.player');
-
-    // Para cada botão (player) encontrado, adiciona um evento de clique
-    player.forEach(btn => {
-        btn.addEventListener('click', function () {
-
-            // Quando o botão é clicado, pega o valor do atributo 'data-target' desse botão
-            const targetId = this.getAttribute('data-target');
-            // Usa esse valor para encontrar um elemento com o ID correspondente
-            const targetBox = document.getElementById(targetId);
-
-            // Seleciona todos os elementos que têm a classe 'box'
-            document.querySelectorAll('.box').forEach(box => {
-                // Remove a classe 'ativo' de todos os elementos 'box'
-                box.classList.remove('ativo');
-            });
-
-            // Adiciona a classe 'ativo' ao 'targetBox', que é o elemento que queremos mostrar
-            targetBox.classList.add('ativo');
-
-
-        });
-
+    document.addEventListener('click', ({ target }) => {
+        const player = target.closest('.player');
+        
+        if (player) {
+            hideAllBoxes();
+            document.getElementById(player.dataset.target).classList.add('ativo');
+        } else if (target.classList.contains('back')) {
+            hideAllBoxes();
+        }
     });
-
-
 });
-
-const back = document.querySelectorAll('.back')
-
-back.forEach(item => {
-    item.addEventListener('click', () => {
-        document.querySelectorAll('.box').forEach(box => {
-            // Remove a classe 'ativo' de todos os elementos 'box'
-            box.classList.remove('ativo');
-        });
-
-    })
-})
-
-
-
-
-
